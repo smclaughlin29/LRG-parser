1257fadc1a754ce47ef9ec0c5a86f59c1accb809
Stef Piatek <stefan@piatek.co.uk>
2017-11-09 10:16:01 +0000

    Streamline getting mapping_list

--- scratch.py
+++ scratch.py
@@ -6,14 +6,8 @@ file = "project_resources/LRG_5.xml"
 tree = ET.parse(file)
 root = tree.getroot()
 
-annotation_set = root.find('updatable_annotation').findall('annotation_set')
-
-annotations = []
-
-for item in annotation_set:
-    if "lrg" in item.attrib['type']:
-        annotations.extend(item.findall('mapping'))
+mapping_list = (root.find("updatable_annotation")
+                    .find("annotation_set[@type='lrg']")
+                    .findall("mapping"))
 
 
-print(GRCh37)
-print(GRCh38)
