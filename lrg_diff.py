0b9335c1522f4d68550f67e902df308830c78450
Shane McLaughlin <shane.mclaughlin@belfasttrust.hscni.net>
2017-11-09 12:45:43 +0000

    added mapping_diff

--- lrg_diff.py
+++ lrg_diff.py
@@ -1,3 +1,5 @@
+from xml.etree import ElementTree as ET
+
 
 def choose_file(name: str) -> str:
     """Takes in LRG or gene name and returns LRG file name 
@@ -7,6 +9,32 @@ def choose_file(name: str) -> str:
     name = name.upper()
     if name.startswith('LRG_'):
         filename = f"lrg_data/{name}.xml"
- 
+    else:
+        # try to lookup
 
     return filename
+
+
+def mapping_diff(filename):
+
+    try:
+        tree = ET.parse(filename)
+    except IOError:
+        raise IOError(f"{filename} does not exist. Please enter correct LRG name")
+
+    root = tree.getroot()
+
+    mapping_list = (root.find("updatable_annotation/annotation_set[@type='lrg']")
+                        .findall("mapping"))
+
+    builds = {}
+
+    for item in mapping_list:
+        builds[item.attrib['coord_system']] = item.attrib
+
+    assert builds, "No mapping co-ordinates found"
+
+    return builds
+
+
+
